e7fb1d71b158d4f966bba05a94a86edcb3e009f7
EJC <122042676@upq.edu.mx>
2026-06-23 18:29:00 -0600

    Practica 11 EJC
    Press y switch

--- practica5/miapp2/screens/PressScreen.js
+++ practica5/miapp2/screens/PressScreen.js
@@ -1,24 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Switch} from 'react-native';
+import { useState } from 'react';
 
-export default function SafeAreaScreen() {
+export default function PressableScreen() {
+  const [buttonText, setButtonText] = useState("Dame Click");
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
+    <View
+      style = {[styles.container, {backgroundColor:isDarkMode ? "#000000":"#ffffff"}]}
+    >
 
-      <View style={styles.container}>
+    <StatusBar style = {isDarkMode ? "light":"dark"}/>
 
-      <Text> Aqui va la practica de Migue </Text>
+      <Pressable style={styles.button}
+  
+        onPress={()=> {
+        console.log("se presiono el boton");
+        setButtonText("Boton Presionado");
+        }}
 
-      <StatusBar style="auto" />
+        onPressIn={() => { 
+          console.log("Se acaba de presionar el boton");
+        }}
+
+        onPressOut={() =>{
+          console.log("Se acaba de soltar el boton");
+        }}
+
+        onLongPress={() =>{
+          console.log("Presion prolongada");
+          setButtonText("Presion prolongada detectada");
+        }}
+
+        >
+
+        <Text style = {styles.buttonText}> {buttonText} </Text>
+
+      </Pressable>
+
+      <Text
+      style={[styles.text, {color: isDarkMode ? "#ffffff":"#000000"}]}
+      >
+        Dark Mode
+      </Text>
+
+      <Switch
+      
+        value={isDarkMode}
+        onValueChange={(previousState) => setIsDarkMode((previousState) => !previousState)}
+        trackColor={{false:"#767577", true:"lightblue"}}
+        thumbColor="#f4f3f4"
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row'
-  },
+    container: {
+        flex: 1, 
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 10,
+        marginBottom: 50 
+    },
+    buttonText: {
+        fontSize: 20,
+        color: "white",
+        textAlign: "center"
+    },
+    switchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "80%", 
+        paddingHorizontal: 10
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "bold"
+    }
 });
