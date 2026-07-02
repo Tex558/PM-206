15eef53650efccddc4ac5c0e32124bbb73013f53
EJC <122042676@upq.edu.mx>
2026-07-02 16:42:57 -0600

    Practica 17 EJC
    BottomSheet y Modal

--- practica5/miapp2/screens/ModalScreen.js
+++ practica5/miapp2/screens/ModalScreen.js
@@ -1,24 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Modal, Pressable} from 'react-native';
+import React, {useState} from 'react';
 
-export default function SafeAreaScreen() {
-  return (
+export default function ModalScreen() {
 
-      <View style={styles.container}>
+  const [modalVisible, setModalVisible] = useState(false);
 
-      <Text> Aqui va la practica de Rafa </Text>
+  return(
+    <View style ={styles,container}>
+      <Text stule = {style.titulo}> Ejemplo de Modal y BottomSheet </Text>
 
-      <StatusBar style="auto" />
+      <Button
+      title='Abrir modal'
+      onPress={() => setModalVisible(true)}
+      />
+
+      <Modal
+      animationType='slide'
+      transparent = {true}
+      visible = {modalVisible}
+      >
+        <View style = {styles.fondo}>
+          <View style = {styles.bottomSheet}>
+            <Text style = {styles.texto}> Hola, esto es un bottomSheet </Text>
+          </View>
+
+          <Pressable 
+          style = {styles.boton}
+          onPress={() => setModalVisible(false)}
+          >
+            <Text style = {styles.textoBoton}> Cerrar </Text>
+          </Pressable>
+
+        </View>
+        
+      </Modal>
+
     </View>
-  );
+  )
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'row'
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  fondo: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  },
+  bottomSheet: {
+    backgroundColor: '#fff',
+    padding: 25,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    alignItems: 'center',
+  },
+  texto: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  boton: {
+    backgroundColor: '#2196F3',
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    borderRadius: 8,
+  },
+  textoBoton: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
