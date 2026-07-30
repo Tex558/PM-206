cfff30063dec036cfb170d25f0c65354f1def4d9
EJC <122042676@upq.edu.mx>
2026-07-30 15:46:38 -0600

    Practica 20 EJC

--- PracticaAM/usuarioApi/screens/AltaUsuariosScreen.js
+++ PracticaAM/usuarioApi/screens/AltaUsuariosScreen.js
@@ -1,15 +1,53 @@
+import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
-import {View,SafeAreaView,Text,TextInput,Pressable,StyleSheet,} from 'react-native';
+import { View, SafeAreaView, Text, TextInput, Pressable, StyleSheet, Alert, Platform } from 'react-native';
 
 export default function App() {
   const [nombre, setNombre] = useState('');
   const [edad, setEdad] = useState('');
+  const router = useRouter();
+  const [cargando, setCargando] = useState(false);
+
+  const mostrarMensaje = (titulo, mensaje) => {
+    if (Platform.OS === 'web') {
+      window.alert(`${titulo}\n ${mensaje}`);
+    } else {
+      Alert.alert(titulo, mensaje);
+    }
+  };
+
+  const guardarUsuario = async () => {
+    if (nombre.trim() === '' || edad.trim() === '') {
+      mostrarMensaje('vacios', 'Completa todos los campos.');
+      return;
+    }
+
+    try {
+      setCargando(true);
+      const respuesta = await fetch('https://thin-roses-cut.loca.lt/v1/usuarios/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nombre: nombre, edad: Number(edad) })
+      });
+      const datos= await respuesta.json();
+      console.log("Respuesta API", datos);
+      mostrarMensaje("Exito","Usuario registrado");
+
+      setNombre('');
+      setEdad('');
+
+
+    } catch (error) {
+      console.log("Error API",error);
+      mostrarMensaje("Error","No fue posible guardar")
+    } finally {
+      setCargando(false);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-
       <View style={styles.card}>
-
         <Text style={styles.titulo}>
           Registro de Usuarios
         </Text>
@@ -29,20 +67,17 @@ export default function App() {
           onChangeText={setEdad}
         />
 
-        <Pressable style={styles.boton}>
+        <Pressable style={styles.boton} onPress={guardarUsuario} disabled = {cargando} >
           <Text style={styles.textoBoton}>
-            Agregar Usuario
+            {cargando? "Guardando...":"Agregar Usuario"} 
           </Text>
         </Pressable>
-
       </View>
-
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#F5F7FA',
@@ -50,7 +85,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
-
   card: {
     width: '100%',
     backgroundColor: '#FFFFFF',
@@ -65,7 +99,6 @@ const styles = StyleSheet.create({
       height: 3,
     },
   },
-
   titulo: {
     fontSize: 26,
     fontWeight: 'bold',
@@ -73,7 +106,6 @@ const styles = StyleSheet.create({
     marginBottom: 25,
     color: '#1F2937',
   },
-
   input: {
     height: 50,
     borderWidth: 1,
@@ -84,7 +116,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#F9FAFB',
     fontSize: 16,
   },
-
   boton: {
     backgroundColor: '#29bb0c',
     paddingVertical: 15,
@@ -92,11 +123,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 10,
   },
-
   textoBoton: {
     color: '#FFFFFF',
     fontSize: 17,
     fontWeight: 'bold',
   },
-
 });
--- PracticaAM/usuarioApi/screens/ConsultaUsuariosScreen.js
+++ PracticaAM/usuarioApi/screens/ConsultaUsuariosScreen.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import {SafeAreaView,View,Text,FlatList,StyleSheet,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, FlatList, StyleSheet, Platform } from 'react-native';
 
-export default function ConsultaUsuariosScreen() {
+export default function ConsultarUsuariosScreen() {
+  const API_URL = 'https://thin-roses-cut.loca.lt';
 
-  const usuarios = [
-    { id: '1', nombre: 'Isay Guerra', edad: 22 },
-    { id: '2', nombre: 'Ana López', edad: 19 },
-    { id: '3', nombre: 'Carlos Gonzalez', edad: 25 },
-    { id: '4', nombre: 'Bjork Guerra', edad: 21 },
-    { id: '5', nombre: 'Luisa Martínez', edad: 28 },
-  ];
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    const fetchUsuarios = async () => {
+      try {
+        const respuesta = await fetch(`${API_URL}/v1/usuarios/`);
+        const datos = await respuesta.json();
+        console.log("Respuesta API:", datos);
+        setUsuarios(datos.usuarios);
+      } catch (error) {
+        console.error('Error API:', error);
+      }
+    };
+
+    fetchUsuarios();
+  }, []);
 
   const renderTarjeta = ({ item }) => (
     <View style={styles.card}>
-
       <Text style={styles.nombre}>{item.nombre}</Text>
 
       <View style={styles.linea}></View>
@@ -22,39 +30,32 @@ export default function ConsultaUsuariosScreen() {
       <Text style={styles.info}>
         Edad: {item.edad} años
       </Text>
-
     </View>
   );
 
   return (
-
     <SafeAreaView style={styles.container}>
-
       <Text style={styles.titulo}>
         Lista de Usuarios
       </Text>
 
       <FlatList
         data={usuarios}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item, index) => item.id ? item.id.toString() : index.toString()}
         renderItem={renderTarjeta}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 20 }}
       />
-
     </SafeAreaView>
   );
-  
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#F5F7FA',
     padding: 20,
   },
-
   titulo: {
     fontSize: 28,
     fontWeight: 'bold',
@@ -62,14 +63,12 @@ const styles = StyleSheet.create({
     color: '#1F2937',
     marginBottom: 20,
   },
-
   card: {
     backgroundColor: '#FFFFFF',
     borderRadius: 15,
     padding: 18,
     marginBottom: 15,
     elevation: 4,
-
     shadowColor: '#000',
     shadowOpacity: 0.15,
     shadowRadius: 5,
@@ -78,22 +77,18 @@ const styles = StyleSheet.create({
       height: 3,
     },
   },
-
   nombre: {
     fontSize: 20,
     fontWeight: 'bold',
     color: '#2563EB',
   },
-
   linea: {
     height: 1,
     backgroundColor: '#E5E7EB',
     marginVertical: 10,
   },
-
   info: {
     fontSize: 16,
     color: '#4B5563',
   },
-
 });
